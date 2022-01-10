b272717ae3f771e5d6a5db081c18c31f3dc11651
Egor1515 <96645276+Egor1515@users.noreply.github.com>
2022-01-10 15:44:50 +1000

    Initial commit

---
+++ src/MilesForFlight.java
@@ -0,0 +1,10 @@
+public class MilesForFlight {
+    public static void main(String[] args) {
+
+        int ticketCost = 20000;
+        int rubPerMile = 20;
+        int bonus = ticketCost / rubPerMile;
+        System.out.println(" Накопленные мили " + bonus);
+    }
+}
+
